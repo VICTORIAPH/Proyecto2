dde072595cf9361974aaf21623528a492f6926fc
VICKYPH <vicky454ph@gmail.com>
2019-06-02 16:42:49 -0500

    menu

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,3 +1,30 @@
-public class Estudiante {
+public class Estudiante  {
 
+    private String nombre;
+    private int numeroControl;
+
+
+
+    public Estudiante(int numeroControl, String nombre){
+        this.nombre=nombre;
+        this.numeroControl=numeroControl;
+    }
+    public String getNombre() {
+
+
+
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroControl() {
+        return numeroControl;
+    }
+
+    public void setNumeroControl(int numeroControl) {
+        this.numeroControl = numeroControl;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,6 +7,44 @@ public class Main {
     }
 
 
+
+
+int i, opcion;
+    while(i==0){
+
+        Scanner teclado =new Scanner(System.in);
+        System.out.println("Que deseas hacer? +\n+"+
+                "1.- Crear +\n+" +
+                "2.-Leer +\n+" +
+                "3.-Agreagar +\n" +
+                "4.-Consultar +\n" +
+                "5.-Modificar +\n" +
+                "6.-Eliminar +\n" +
+                "7.-Grabar +\n");
+
+        opcion=teclado.nextInt();
+switch (opcion){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+}
+
+
+
+
+    }
 
 
 
