c8b934d9166a143fe3d29ed1e18d92735d4d3ba1
ceo_emmanuel <flotuz10@gmail.com>
2019-06-02 16:47:48 -0500

    Se mejoró el menú

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        menu();
     }
 
-
-
-
-int i, opcion;
-    while(i==0){
-
-        Scanner teclado =new Scanner(System.in);
+    private static void menu() {
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Que deseas hacer? +\n+"+
                 "1.- Crear +\n+" +
                 "2.-Leer +\n+" +
@@ -22,30 +17,28 @@ int i, opcion;
                 "6.-Eliminar +\n" +
                 "7.-Grabar +\n");
 
-        opcion=teclado.nextInt();
-switch (opcion){
-    case 1:
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-
-    case 4:
-        break;
-    case 5:
-        break;
-    case 6:
-        break;
-    case 7:
-        break;
-}
-
-
-
-
+        switch (teclado.nextInt()){
+            case 1:
+                menu();
+                break;
+            case 2:
+                menu();
+                break;
+            case 3:
+                menu();
+                break;
+            case 4:
+                menu();
+                break;
+            case 5:
+                menu();
+                break;
+            case 6:
+                menu();
+                break;
+            case 7:
+                menu();
+                break;
+        }
     }
-
-
-
 }
