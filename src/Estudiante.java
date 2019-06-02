f5ebbf77bc2c130d419ad8635b9e21a43b1bb377
VICKYPH <vicky454ph@gmail.com>
2019-06-02 17:16:21 -0500

    crud 2

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,18 +1,18 @@
 public class Estudiante  {
 
     private String nombre;
-    private int numeroControl;
+    private int NumerodeControl;
 
 
 
     public Estudiante(int numeroControl, String nombre){
         this.nombre=nombre;
-        this.numeroControl=numeroControl;
+        this.NumerodeControl=numeroControl;
     }
 
     @Override
     public String toString() {
-        return "Soy "+nombre+" con numero de control: "+numeroControl;
+        return "Soy "+nombre+" con numero de control: "+NumerodeControl;
     }
 
     public String getNombre() {
@@ -23,11 +23,11 @@ public class Estudiante  {
         this.nombre = nombre;
     }
 
-    public int getNumeroControl() {
-        return numeroControl;
+    public int getNumerodeControl() {
+        return NumerodeControl;
     }
 
     public void setNumeroControl(int numeroControl) {
-        this.numeroControl = numeroControl;
+        this.NumerodeControl = numeroControl;
     }
 }
